e962471af15e2112b66e8520808d559b7b23426e
kaustubh-s1 <shuklakaustubh84@gmail.com>
2023-06-02 00:50:19 +0530

    👀 add GET api to retrieve elevator systems and elevator

--- src/urls.py
+++ src/urls.py
@@ -6,4 +6,8 @@ from .views import *
 urlpatterns = [
     # Create new elevator systems
     path('elevate/add-system',CreateElevatorSystem.as_view(),name='add-elevator-sys'),
+    # Show all the elevator systems
+    path('elevate/show-all/',ElevatorSystemList.as_view(),name='show-all-sys'),
+    # Show all elevators belonging to an elevator system
+    path('elevate/<int:id>/show/',ElevatorsList.as_view(),name='elevator-list'),
 ]
--- src/views.py
+++ src/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 
-from .models import ElevatorSystem
+from .models import Elevator, ElevatorSystem
 
-from .serializers import ElevatorSystemSerializer
+from .serializers import ElevatorSerializer, ElevatorSystemSerializer
 from utils.initiate_elevators import initiate_elevators
 
 
@@ -21,3 +21,12 @@ class CreateElevatorSystem(generics.CreateAPIView):
             number_of_elevators=serializer.data['number_of_elevators'],
             system_id=elevator_system.id
         )
+        
+class ElevatorsList(generics.ListAPIView):
+    serializer_class = ElevatorSerializer
+
+    def get_queryset(self):
+        system_id = self.kwargs['id']
+        queryset = Elevator.objects.filter(elevator_system__id = system_id)
+
+        return queryset
